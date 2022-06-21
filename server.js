85c7fed5d3c89400072799ec2225e7a62632e446
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-06-21 19:57:04 +0900

    feat: mini Chatting

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+const socketIO = require("socket.io");
+const moment = require("moment");
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "src")));
+const PORT = 3500;
+
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    const { name, msg } = data;
+    io.emit("chatting", {
+      name,
+      msg,
+      time: moment(new Date()).format("hh:mm A"),
+    });
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
